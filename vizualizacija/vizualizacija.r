ada1b1373ced8bdd22e6e9aed722748eb942b29c
Klara Travnik <travnik.klara@gmail.com>
2022-01-13 19:17:46 +0100

    naredila zemljevide

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,30 +1,21 @@
 # 3. faza: Vizualizacija podatkov
-library(readr)
-library(stringr)
-library(dplyr)
-library(tidyr)
-library(tidyverse)
-library(xml2)
-library(rvest)
 
-library(ggplot2)
-library(dplyr)
-library(plotly)
-library(sp)
-library(rgdal)
-
+source("lib/libraries.r")
 
 #1. graf:Povprečni pridelek glede na število hektarjev \n kmetijskih površin po regijah
 ggplot(zemljisca.in.pridelek %>% filter(regija != "slo")) + 
   aes(x = leto, y = skupni.povprecni.pridelek, size = skupno.stevilo.zemljisc) +
   labs(title = "Povprečni pridelek glede na število hektarjev \n kmetijskih površin po regijah") + xlab("Leto") + ylab("Povprečni pridelek") +
   labs(size = "Skupno število zemljišč") +
-  geom_point(color="darkblue") + facet_wrap(.~regija) +
+  geom_point(color="darkblue") +
   theme(legend.title = element_text(color = "darkblue", size = 10),
         axis.line = element_line(colour = "darkblue", 
                                  size = 1, linetype = "solid"),
         legend.background = element_rect(fill = "lightblue", linetype="solid", 
-                                         colour ="darkblue"), plot.title = element_text(color = "darkblue", hjust=0.5), strip.background=element_rect(colour="black",fill="white")) 
+                                         colour ="darkblue"), plot.title = element_text(color = "darkblue", hjust=0.5), strip.background=element_rect(colour="black",fill="white"),
+        axis.text.x = element_text(angle = 45, vjust = 0.5)) +
+  facet_wrap(.~regija)
+
 
 
 
@@ -36,12 +27,13 @@ ggplot(podatki2) +
   xlab("Vrsta pridelka")+ ylab("Povprečni pridelek") + 
   labs(title = "Primerjava povprečnih pridelkov v letih 2010 in 2020")+
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
-    axis.title.x = element_text(vjust = 0)) + facet_wrap(.~leto) + 
-  theme(plot.title = element_text(color = "darkblue", hjust=0.5), strip.background=element_rect(colour="black",fill="white")) 
+    axis.title.x = element_text(vjust = 0), 
+  plot.title = element_text(color = "darkblue", hjust=0.5), strip.background=element_rect(colour="black",fill="white")) + 
+  facet_wrap(.~leto)
 
 
 
-#2. graf:Število živine glede na  površino  kmetijskih zemljišč večjih slovenskih regij
+#3. graf:Število živine glede na  površino  kmetijskih zemljišč večjih slovenskih regij
 ggplot(zemljisca.in.zivina %>% filter(regija %in% c("lj", "nm", "mb", "ms", "kp", "kr"))) + aes(x = skupno.stevilo.zemljisc, y = skupno.stevilo.zivine, color = leto, size = zivina.na.ha) +
   labs(title = "Število živine glede na  površino \n kmetijskih zemljišč večjih slovenskih regij") + xlab("Površina zemljišč") + ylab("Število živine") +
   labs(size="Živina na ha", color="Leto") + 
@@ -65,74 +57,55 @@ ggplotly(graf2, tooltip = "text")
 
 
 
-#3. graf:
-#ggplot(prodaja.in.potrosnja) + aes(x = leto, y = razmerje) +
- # xlab("Leto") + ylab("Razmerje potrošnja : prodaja") +
- # geom_col(fill="darkred") + facet_wrap(.~pridelek) + 
- # labs(title = "Razmerje med potrošnjo in prodajo živil") +
- # theme(plot.title = element_text(color = "darkred", hjust=0.5, size=15), legend.position="none",
-   #     strip.background=element_rect(colour="black", fill="white"))
 
-
+#4. graf
+#uredi še labele zunaj diagrama
 #tortni diagram
 
-ggplot(prodaja.in.potrosnja %>% filter(leto == 2020), aes(x="", y=razmerje, fill=pridelek)) +
+podatki3 <- prodaja.in.potrosnja %>% filter(leto == 2020)
+ggplot(podatki3, aes(x="", y=razmerje, fill=pridelek)) +
   geom_bar(stat="identity", width=1, color="white") +
   coord_polar("y", start=0) + theme_void() +
-  scale_fill_brewer(palette="Set1") + 
+  scale_fill_hue() + 
   labs(title = "Razmerje med potrošnjo in prodajo živil v letu 2020") +
   labs(fill = "Pridelek") + 
-  theme(plot.title = element_text(color = "darkred", hjust=0.5, size=15, vjust = -8),
-        legend.title = element_text(color = "darkred", size = 12),
-        legend.background = element_rect(fill = "white", linetype="solid", 
-                                         colour ="darkred"))
+  theme(plot.title = element_text(color = "darkblue", hjust=0.5, size=15, vjust = -8),
+        legend.title = element_text(color = "darkblue", size = 12),
+        legend.background = element_rect(fill = "lightblue", linetype="solid", 
+                                         colour ="darkblue")) 
   
 
+#pie(podatki3$razmerje, labels = podatki3$pridelek)
 
 
-#4. graf:
+#5. graf:
 
 ggplot(samooskrba.in.poraba) + aes(x = leto, y = stopnja.samooskrbe,color = zmoznost) + 
   labs(title = "Samooskrba in preskrba z živili") + xlab("Leto") + ylab("Stopnja samooskrbe (v %)") + 
   theme(legend.position = "right") + labs(color = "Zmožnost samooskrbe") +
   geom_line(size=2) + facet_wrap(.~zivila) + 
-  theme(plot.title = element_text(color = "darkred", hjust=0.5, size=15)) + 
-  theme(strip.background=element_rect(colour="black",fill="white"), legend.title = element_text(color = "darkred", size = 10),
-        legend.background = element_rect(fill = "white", linetype="solid", colour ="darkred"), axis.text.x = element_text(angle = 45, hjust = 1, colour = "darkred")) + 
+  theme(plot.title = element_text(color = "darkblue", hjust=0.5, size=15)) + 
+  theme(strip.background=element_rect(colour="black",fill="white"), legend.title = element_text(color = "darkblue", size = 10),
+        legend.background = element_rect(fill = "lightblue", linetype="solid", colour ="darkblue"), axis.text.x = element_text(angle = 45, hjust = 1, colour = "darkblue")) + 
   scale_color_manual(values=c("lightgreen", "orange", "red"))
 
 
+#6. graf:
+ggplot(samooskrba.in.poraba, aes(poraba, zivila)) + geom_boxplot(fill = "lightblue") + xlab("Poraba") + 
+  ylab("Živila") + 
+  theme(axis.line = element_line(colour = "darkblue", 
+                                 size = 1, linetype = "solid"),
+        axis.text.y = element_text(colour = "darkblue")) + xlim(0, 130)
 
-
-#graf6 = ggplot(samooskrba.in.poraba) + 
- # aes(x = leto, y = stopnja.samooskrbe, size = preskrba, color = zivila, 
-    #  text = paste(
-    #    "Živilo: ", zivila, "\n",
-    #    "Zmožnost samooskrbe: ", zmoznost,
-    #    sep = ""
-   #   )) + ggtitle("Samooskrba in preskrba z živili") + xlab("Leto") + ylab("Stopnja samooskrbe (v %)") + 
- # theme(legend.position = "bottom") + labs(size="Preskrba (velikost)", color = "Živila") +
- # geom_point()
-#ggplotly(graf6, tooltip = "text")
-
-
-#5. graf:
-podatki <- zivina3 %>% filter(regija != "slo") %>% group_by(vrsta.zivine, leto) %>% mutate(st.zivine = sum(stevilo.zivine)) %>%
-  select(leto, vrsta.zivine, st.zivine)
-podatki <- distinct(podatki)
-
-ggplot(podatki) + 
-  aes(x = vrsta.zivine, y = st.zivine) + 
-  geom_point() + facet_wrap(.~leto)
 
 
 ########################################## ZEMLJEVIDI #########################################################
 
 
-zemljevid <- readOGR("podatki/SLO/gadm36_SVN_1.shp", encoding = "UTF-8")
+zemljevid <- readOGR("podatki/SLO/gadm36_SVN_1.shp")
 zemljevid <- zemljevid %>% spTransform(CRS("+proj=longlat +datum=WGS84")) # pretvorimo v ustrezen format
 
-loc <- locale(encoding="Windows-1250")
+loc <- locale(encoding="UTF-8")
 for (col in names(zemljevid)) {
   if (is.character(zemljevid[[col]])) {
     zemljevid[[col]] <- zemljevid[[col]] %>% parse_character(locale=loc)
@@ -144,9 +117,100 @@ zemljevid.regije <- zemljevid
 names(zemljevid.regije)
 zemljevid.regije$NAME_1
 
-source("lib/uvozi.zemljevid.r")
 
-zemljevid2 <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
-                              "SVN_adm1", encoding = "UTF-8") %>% fortify()
+
+zemljevid.regije$NAME_1 <- factor(zemljevid.regije$NAME_1)
+
+lvls <- levels(zemljevid.regije$NAME_1)
+
+# spremenim imena regij, da se ujemajo z zemljevidom, kratice spremenim nazaj v dolga imena
+
+imena.regij.nazaj = tibble(
+  oznaka = c(
+    "Gorenjska",
+    "Goriška",
+    "Jugovzhodna Slovenija",
+    "Koroška",
+    "Obalno-kraška",
+    "Osrednjeslovenska",
+    "Podravska",
+    "Pomurska",
+    "Spodnjeposavska",
+    "Notranjsko-kraška",
+    "Savinjska",
+    "Zasavska",
+    "SLOVENIJA"
+  ),
+  regija = c(
+    "kr", "ng", "nm", "sg", "kp", "lj", "mb", "ms", "kk", "po", "ce", "za", "slo"
+  )
+)
+
+#Primerjava števila goveda v regijah leta 2003 in 2016
+tmap_mode("plot")
+zivina3.za.zemljevid <- merge(x = zivina3, y = imena.regij.nazaj, by = "regija", all.x = TRUE) %>% 
+  dplyr::select(regija = oznaka, vrsta.zivine, leto, stevilo.zivine) %>% filter(regija != "SLOVENIJA")
+
+
+zivina3.za.zemljevid.2003 <- zivina3.za.zemljevid %>% filter(leto == 2003, vrsta.zivine == "govedo") %>% group_by(regija, leto) 
+graf.zemljevid0 <- merge(zemljevid.regije, zivina3.za.zemljevid.2003, by.x = "NAME_1", by.y = "regija")
+tm_shape(graf.zemljevid0) + tm_polygons("stevilo.zivine" , popup.vars = c("Število goveda: " = "stevilo.zivine"),
+                                       style = "pretty", palette="Blues",title="Število goveda") + 
+  tm_layout(
+    "Število goveda v letu 2003",
+    legend.title.size=1,
+    legend.text.size = 0.8,
+    legend.position = c("right","bottom"),
+    legend.bg.color = "white",
+    legend.bg.alpha = 1)
+
+
+zivina3.za.zemljevid.2016 <- zivina3.za.zemljevid %>% filter(leto == 2016, vrsta.zivine == "govedo") %>% group_by(regija, leto) 
+graf.zemljevid <- merge(zemljevid.regije, zivina3.za.zemljevid.2016, by.x = "NAME_1", by.y = "regija")
+tm_shape(graf.zemljevid) + tm_polygons("stevilo.zivine" , popup.vars = c("Število goveda: " = "stevilo.zivine"),
+                                       style = "pretty", palette="Blues",title="Število goveda") +
+  tm_layout(
+    "Število goveda v letu 2016",
+    legend.title.size=1,
+    legend.text.size = 0.8,
+    legend.position = c("right","bottom"),
+    legend.bg.color = "white",
+    legend.bg.alpha = 1)
+
+
+
+#Primerjava hektarjev gozdov leta 2003 in 2016
+zemljisca.za.zemljevid <- merge(x = zemljisca, y = imena.regij.nazaj, by = "regija", all.x = TRUE) %>%
+  dplyr::select(regija = oznaka, leto, vrsta.zemljisca, povrsina) 
+
+zemljisca.2003 <- zemljisca.za.zemljevid %>% filter(regija != "SLOVENIJA", vrsta.zemljisca == "gozd", leto == 2003)
+
+graf.zemljevid2 <- merge(zemljevid.regije, zemljisca.2003, by.x = "NAME_1", by.y = "regija")
+
+tm_shape(graf.zemljevid2) + tm_polygons("povrsina" , popup.vars = c("Število hektarjev gozdov " = "povrsina"),
+                                                     style = "pretty", palette="Blues",
+                                                    title="Število hektarjev gozda") +
+  tm_layout(
+    "Število hektarjev gozdov v 2003",
+    legend.title.size=1,
+    legend.text.size = 0.8,
+    legend.position = c("right","bottom"),
+    legend.bg.color = "white",
+    legend.bg.alpha = 1)
+
+zemljisca.2016 <- zemljisca.za.zemljevid %>% filter(regija != "SLOVENIJA", vrsta.zemljisca == "gozd", leto == 2016)
+graf.zemljevid3 <- merge(zemljevid.regije, zemljisca.2016, by.x = "NAME_1", by.y = "regija")
+
+tm_shape(graf.zemljevid3) + tm_polygons("povrsina" , popup.vars = c("Število hektarjev gozdov " = "povrsina"),
+                                        style = "pretty", palette="Blues",
+                                        title="Število hektarjev gozda") +
+  tm_layout(
+    "Število hektarjev gozdov v 2016",
+    legend.title.size=1,
+    legend.text.size = 0.8,
+    legend.position = c("right","bottom"),
+    legend.bg.color = "white",
+    legend.bg.alpha = 1)
+
 
 
