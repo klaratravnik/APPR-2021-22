0358c24d2cf47e92e09d8066b50e59582e24896b
Klara Travnik <travnik.klara@gmail.com>
2022-01-20 18:25:55 +0100

    narejena shiny aplikacija

--- shiny/server.R
+++ shiny/server.R
@@ -1,2 +1,18 @@
 library(shiny)
 
+shinyServer(function(input, output) {
+  
+  output$graf <- renderPlot({
+    narisi_zem(input$leto1,input$vrsta.zemljisca1)
+  })
+})
+
+
+
+narisi_zem = function(leto1, vrsta.zemljisca1){
+    zem = tm_shape(merge(zemljevid.regije, zemljisca.za.zemljevid %>% filter(regija != "SLOVENIJA", leto == leto1, vrsta.zemljisca == vrsta.zemljisca1), by.x = "NAME_1", by.y = "regija")) + 
+      tm_polygons("povrsina", style = "pretty", palette="Blues", title = vrsta.zemljisca1)
+    print(zem)
+}
+
+
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,1 +1,21 @@
 library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel(""),
+  sidebarLayout(position = "left",
+                sidebarPanel(
+                  selectInput(
+                    "leto1",
+                    label = "Leto:",
+                    choices = c(2003, 2005, 2007, 2010, 2013, 2016),
+                    selected = 2010
+                  ),
+                  selectInput(
+                    "vrsta.zemljisca1",
+                    label = "Vrsta zemljišča:",
+                    choices = c("gozd", "nerodovitno", "njive", "travniki in pašniki", "žita"),
+                    selected = "gozd"
+                  ))
+                ,
+                mainPanel(plotOutput("graf"))),
+  uiOutput("izborTabPanel")))
